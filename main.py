36fb8b65f5f2371d61557007fb18afb2482d4def
Saurav Bista <sauravbista10@gmail.com>
2024-07-23 06:58:32 +0545

    Update main.py
    Added message box, when user inputs a false name

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ turtle_2 = Turtle()
 turtle_2.penup()
 turtle_2.shape()
 
+from tkinter import messagebox
 screen = Screen()
 
 screen.title("This is US states guessing game")
@@ -27,22 +28,23 @@ with open("./50_states.csv") as get_cor:
     while len(guessed_state) < 50:
         input_states = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="What's the state name?").title()
         user_input = ask_input()
-        if user_input in states_name:
+        if user_input == "Exit":
+            missing = [state for state in states_name if state not in guessed_state]
+            new_data = pandas.DataFrame(missing)
+            new_data.to_csv("states_to_learn")
+            screen.bye()
+            break
+        elif user_input in states_name:
             guessed_state.append(user_input)
             index = data[data.state == user_input]
             turtle_2.goto((int(index.x), int(index.y)))
             turtle_2.pendown()
             turtle_2.write(user_input, False, font=("Arial", 8, "normal"))
             turtle_2.penup()
-        elif user_input == "Exit":
-            missing = [state for state in states_name if state not in guessed_state]
-            new_data = pandas.DataFrame(missing)
-            new_data.to_csv("states_to_learn")
-            break
 
         else:
+            messagebox.showerror("State not found", f"There is no such state as {user_input}")
 
-            print("State Not found, next guess!!!")
 
 
 turtle.screen.mainloop()
